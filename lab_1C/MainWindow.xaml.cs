a2b988eeaf00601754dc8a42c0638118d7efcf8a
KarolinaWozniak99 <karolina.wozniak99@wp.pl>
2020-03-28 20:01:28 +0100

    lista

---
+++ lab_1C/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab_1C
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            TextBoxWithErrorProvider.BrushForAll = Brushes.Green;
+            InitializeComponent();
+            
+            
+           
+        }
+
+        private bool IsNotEmpty(TextBoxWithErrorProvider tb)
+        {
+            if (tb.Text.Trim() == "")
+            {
+                tb.SetError("Pole nie może być puste!");
+                return false;
+            }
+            tb.SetError("");
+            return true;
+        }
+
+        private void buttonOk_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsNotEmpty(textBoxW) & IsNotEmpty(textBoxW1))
+            {
+                listBoxPilkarze.Items.Add(new Pilkarz("Robert","Lewandowski",30,80));
+            }
+            
+        }
+    }
+}
---
+++ lab_1C/Pilkarz.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1C
+{
+    internal class Pilkarz
+    {
+        //private string _imie;
+
+        //public string Imie {
+
+        //    get { return _imie; }
+        //    set { _imie = value; }
+        //}
+        #region Prop
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public uint Wiek { get; set; }
+        public uint Waga { get; set; }
+        #endregion
+
+        #region constr
+        public Pilkarz(string imie, string nazwisko, uint wiek, uint waga)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Wiek = wiek;
+            Waga = waga;
+        }
+
+        public Pilkarz(string imie, string nazwisko) : this(imie, nazwisko, 30, 75) { }
+        #endregion
+
+        #region methods
+
+        public override string ToString()
+        {
+            return $"{Nazwisko} {Imie} lat: {Wiek} waga: {Waga} kg";
+        }
+
+        #endregion
+    }
+}
